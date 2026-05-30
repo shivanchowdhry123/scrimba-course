8b72f3aaf7bc9c61089d897d40aa330b2632fe46
shivanchowdhry123 <chowdhryshivan@gmail.com>
2026-05-30 18:19:35 +0000

    3.1.5

--- 03. JavaScript Fundamentals/01. Build a Counter App/05. Reassigning and incrementing/index.js
+++ 03. JavaScript Fundamentals/01. Build a Counter App/05. Reassigning and incrementing/index.js
@@ -5,7 +5,15 @@ count = count + 1
 console.log(count)
 
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+let bonusPoints = 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 100
+console.log(bonusPoints)
 // Decrease it down to 25, and then finally increase it to 70
+bonusPoints = bonusPoints + 25
+console.log(bonusPoints)
+bonusPoints = bonusPoints - 70
+console.log(bonusPoints)
 // Console.log the value after each step
 
 
