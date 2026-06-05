73e92aad9cb0d67e484dc2af11a1fb0730609dac
shivanchowdhry123 <chowdhryshivan@gmail.com>
2026-06-05 18:01:47 +0000

    3.1.15&16

--- 03. JavaScript Fundamentals/01. Build a Counter App/16. Create the save button/index.js
+++ 03. JavaScript Fundamentals/01. Build a Counter App/16. Create the save button/index.js
@@ -1,11 +1,13 @@
-let countEl = document.getElementById("count-el")
+// grab the count-el element, store it in a countEl variable
 let count = 0
-
+let countEl = document.getElementById("count-el")
 function increment() {
     count = count + 1
+    // set countEl's innerText to the count
     countEl.innerText = count
 }
-
 // 1. Create a function, save(), which logs out the count when it's called
 
-
+function save() {
+    console.log(count)
+}
