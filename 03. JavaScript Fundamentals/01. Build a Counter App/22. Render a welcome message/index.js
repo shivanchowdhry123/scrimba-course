e5426e425905f294c2b150aefd12a3a4e4e612a3
shivanchowdhry123 <chowdhryshivan@gmail.com>
2026-06-05 18:31:39 +0000

    3.1.22

--- 03. JavaScript Fundamentals/01. Build a Counter App/22. Render a welcome message/index.js
+++ 03. JavaScript Fundamentals/01. Build a Counter App/22. Render a welcome message/index.js
@@ -1,6 +1,9 @@
 // Grab the welcome-el paragraph and store it in a variable called welcomeEl
+let welcomeEl = document.getElementById('welcome-el')
 
 // Create two variables (name & greeting) that contains your name
+let name = 'shivan'
 // and the greeting we want to render on the page
-
+let greeting = 'Welcome Back , '
 // Render the welcome message using welcomeEl.innerText
+welcomeEl.innerText = greeting + name
